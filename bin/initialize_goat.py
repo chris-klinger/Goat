0d60085aa5f5d6e1507566b18e96afa4fd304b1f
Appolloyon <cklinger@ualberta.ca>
2016-07-24 18:52:46 +0100

    Added the main program interface and initialization routine.
    This program is intended to be run in a separate directory, and will
    create a working subdirectory to store its protected files. This will
    include things like settings and the databases used. The main program
    is envisioned as a primarily interactive loop, but could implement a
    script-only implementation as well.

---
+++ bin/initialize_goat.py
@@ -0,0 +1,35 @@
+"""
+Provides an initialization framework for Goat in a new directory.
+
+Consists of the following functions:
+'initialize_goat()' - runs the other two functions
+'check_goat()' - checks whether a Goat subdirectory already exists and
+returns True, else returns False
+'make_goat()' - builds a Goat subdirectory structure if required
+"""
+
+import os
+
+
+def initialize():
+    """Builds a Goat subdirectory if one does not exist"""
+    if not check_goat():
+        make_goat()
+
+def check_goat():
+    """Checks the current directory for a Goat subdirectory"""
+    for elem in os.listdir():
+        if os.path.isdir(elem) and 'Goat' in elem:  # Goat subdir
+            return True
+    return False
+
+def make_goat():
+    """Makes a new Goat subdirectory"""
+    newfiles = ['goat_settings.txt']
+    newdirs = ['Goat_databases', 'Goat_temp']
+    curr_dir = os.getcwd()
+    os.mkdir('Goat')
+    for newfile in newfiles:
+        open(os.path.join(curr_dir, 'Goat', newfile), 'a').close()
+    for newdir in newdirs:
+        os.makedirs(os.path.join(curr_dir, 'Goat', newdir))
