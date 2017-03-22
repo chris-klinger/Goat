213a653fff440f0c969bc9e7efcb9fc7dced65ef
Appolloyon <cklinger@ualberta.ca>
2017-03-22 13:11:16 +0000

    Initial database gui viewer finished.
    Very basic GUI interface implements a two-panel system, with a tree view to the
    database object and a display panel to the right for information about individual
    elements of the database. Currently, the main issue is that long filepaths are
    hidden instead of being wrapped. Minwidth of the label is specified in character
    widths whereas the wraplength is in pixels, so cannot simply wrap to the min length.
    Need to look into trying to configure the wraplength dynamically based on the size
    of the bounding box for the widget.
    
    Next task is to add functionality for adding/removing/modifying records and their
    associated files. Need to carefully consider each operation, such that maximum
    flexibility is afforded to the user.

--- gui/database/database_gui.py
+++ gui/database/database_gui.py
@@ -15,17 +15,12 @@ class DatabaseGui(ttk.Panedwindow):
     def __init__(self, database, parent=None):
         ttk.Panedwindow.__init__(self, parent, orient=HORIZONTAL)
         # assign an instance variable, pass in self as the parent to child widgets
-        self.info_frame = ttk.Labelframe(self,text='info',width=100,height=100)
+        self.info_frame = ttk.Labelframe(self,text='Database Information')
         self.info_frame.pack(expand=YES,fill=BOTH)
         self.info_panel = InfoPanel(self.info_frame)
-        self.db_frame = ttk.Labelframe(self,text='dbs',width=100,height=100)
-        self.db_frame.pack(expand=YES,fill=BOTH)
-        self.db_viewer = DatabaseViewer(database,self.info_panel,self.db_frame)
-        self.add(self.db_frame)
+        self.db_viewer = DatabaseViewer(database,self.info_panel,self)#.db_frame)
+        self.add(self.db_viewer)
         self.add(self.info_frame)
-        #self.add(self.db_viewer)
-        #self.add(self.info_panel)
-        #self.add(Frame(self))#,width=100,height=100))
         self.pack(expand=YES,fill=BOTH)
 
 class DatabaseViewer(ttk.Treeview):
@@ -33,13 +28,13 @@ class DatabaseViewer(ttk.Treeview):
         ttk.Treeview.__init__(self, parent)
         self.db = database
         self.info = info_panel
-        self.selectmode = 'browse' # select one item at a time only
+        self.config(selectmode = 'browse') # select one item at a time only
         self.tag_bind('record', '<Double-1>', # single click doesn't register item properly
                 callback=lambda x:self.itemClicked('record'))
         self.tag_bind('file', '<Double-1>',
                 callback=lambda x:self.itemClicked('file'))
         self.make_tree()
-        self.pack(expand=YES,fill=BOTH)
+        self.pack(side=LEFT,expand=YES,fill=BOTH)
 
     def make_tree(self):
         for record in self.db.list_records():
@@ -63,31 +58,33 @@ class DatabaseViewer(ttk.Treeview):
             self.info.update_info('record', *rlist) # delegates information to display to widget
         elif item_type == 'file':
             flist = []
-            record_name,file_type = item.rsplit('_',1)[0], item.rsplit('_')[1] # chop back off the name
+            record_name,file_type = item.rsplit('_',1)[0], item.rsplit('_',1)[1] # chop back off the name
             record = self.db.fetch_record(record_name)
-            flist.extend((record.genus,record.species,file_type,getattr(record,file_type)))
+            flist.extend((record.genus,record.species,file_type,
+                getattr(record,file_type)))
             self.info.update_info('file', *flist)
 
 class InfoPanel(ttk.Label):
     def __init__(self, parent=None):
         ttk.Label.__init__(self, parent)
-        self.pack(expand=YES,fill=BOTH)
+        self.pack(side=RIGHT,expand=YES,fill=BOTH)
         self.displayInfo = StringVar() # variable watches for updates
-        self.config(textvariable=self.displayInfo)
-        self.displayInfo.set('Goat') # initialize to an empty value
+        self.config(textvariable=self.displayInfo, # associate label text with variable
+                width=-50, # set a sane minimum width)
+                anchor='center',justify='center') # centre and justify label text
+        self.displayInfo.set('') # initialize to an empty value
 
     def update_info(self,display_type,*values):
         display_string = ''
+        display_string += (values[0] + ' ' + values[1] + '\n\n\n\n')
         if display_type == 'record':
-            display_string += (values[0] + ' ' + values[1] + '\n\n')
             if len(values) > 2:
                 for index,value in enumerate(values[2:]):
-                    if index % 2 != 0: # odd value
-                        display_string += (values[index+1] + ' ')
-                    else: # even value
-                        display_string += (values[index+1] + '\n')
+                    if index % 2 == 0: # even value
+                        display_string += (values[index+2] + ' ') # +2 compensates for first two list values
+                    else: # odd value
+                        display_string += (values[index+2] + '\n')
         else:
-            pass
-        #print(display_string)
+            display_string += ('Filetype: ' + values[2] + '\n')
+            display_string += ('Filepath: ' + values[3] + '\n')
         self.displayInfo.set(display_string)
-        #print(self.displayInfo.get())
